7b8ecf44d4bcf69b869f2c232dfcdf15c9076b0d
apolonskyi <apolonskyi@alteryx.com>
2022-10-24 21:16:12 +0200

    Lesson 5 - practice

--- lesson-5/index.js
+++ lesson-5/index.js
@@ -16,6 +16,29 @@
 //   let human3 = createHuman("John", "Fruits", 4);
 
 // function Human(name, favoriteFood, hoursOfSleep) {
+//     return {
+//       legs: 2,
+//       hands: 2,
+//       head: 1,
+//       name,
+//       favoriteFood,
+//       hoursOfSleep,
+//       // Objects can contain functions as well
+//       walk: function() {
+//         console.log("Human is walking!");
+//       },
+//       eat: function() {
+//         console.log("Human is eating!");
+//       },
+//       sleep: function() {
+//         console.log("Human is sleeping!");
+//       }
+//     };
+//   }
+//   let human1 = Human("Mykola", "Borcht", null);
+//   console.log(human1)
+
+// function Human(name, favoriteFood, hoursOfSleep) {
 //     this.someValue = 'test'
 //     return {
 //       legs: 2,
@@ -59,7 +82,7 @@
 //     this.name = name;
 //     this.favoriteFood = favoriteFood;
 //     this.hoursOfSleep = hoursOfSleep;
-//     this.walk = function() {
+//     this.walk = () => {
 //       console.log(`${this.name} is walking`);
 //     };
 //     this.eat = function() {
@@ -73,10 +96,43 @@
 //         console.log(this);
 //         return `Got this: ${Object.keys(this)}`
 //       };
+//     console.log(this)
 //   }
-//   // new Human() syntax will automatically create object for us and return 'this'
-//   // new key means run your function and return done object
+
 //   let human1 = new Human("Mykola", "Borcht", null);
+// //   console.log(human1)
+//   human1.walk()
+//   class Human2 {
+//     constructor(name) {
+//         this.name = name
+//     }
+
+//     walk = () => {
+//         console.log(this.name)
+//     }
+//   }
+  
+
+//   const obj = {
+//     name: 'COnstName',
+//     walkObj: function()  {
+//         const arrFun = () => {
+//             console.log(this);
+//         }
+//         function regularFunc() {
+//             console.log(this)
+//         }
+//         arrFun();
+//         regularFunc();
+//         console.log(this)
+//     }
+//   }
+//   obj.walkObj()
+  // new Human() syntax will automatically create object for us and return 'this'
+  // new key means run your function and return done object
+ 
+//   const humna2 = new Human2('Andrii');
+//   humna2.walk()
 //   let human2 = new Human("Bob", "Pasta", 7);
 //   let human3 = new Human("John", "Fruits", 4);
   
@@ -91,50 +147,67 @@
 //   }
 
 
-// class Human {
-//     // constructor function will be called at object creation
-//     // you don't need to put function key word
-//     // constructor actualy constract your function
-//     #isParent = false
-//     constructor(name, favoriteFood, hoursOfSleep) {
-//       // you can set any your custom properties
-//       this.legs = 2;
-//       this.hands = 2;
-//       this.head = 1;
-//       this.name = name;
-//       this.favoriteFood = favoriteFood;
-//       this.hoursOfSleep = hoursOfSleep || this.hoursOfSleep
-//       this.#isParent = false
-//     }
-//     hoursOfSleep = 7
-//     walk() {
-//       this.someVariable = 'value'; // undefined
-//       return `${this.name} is walking`;
-//     };
-//     eat() {
-//       console.log(`${this.name} is eating ${this.favoriteFood}`);
-//     };
-//     sleep() {
-//       console.log(`${this.name} went sleep for ${this.hoursOfSleep} hours`);
-//     };
+class Human {
+    // constructor function will be called at object creation
+    // you don't need to put function key word
+    // constructor actualy constract your function
 
-//     #getIsParent(){
-//         return this.#isParent;
-//     }
+    constructor(name, favoriteFood, hoursOfSleep) {
+      // you can set any your custom properties
+      this.nameMine = name;
+      this.favoriteFood = favoriteFood;
+      this.hoursOfSleep = hoursOfSleep || this.hoursOfSleep
+    }
+    legs = 2;
+    hands = 2;
+    head = 1;
+    hoursOfSleep = 7
+    #privateHuman = [100,200,300]
+    walk = () => {
+        this.legs = 90;
+        console.log(this)
+        const arrInside = () => {
+            console.log(this)
+        }
+        function defClasic() {
+            console.log(this);
+        }
+        arrInside()
+        defClasic.bind(this)()
+      this.someVariable = 'value'; // undefined
+      return `${this.nameMine} is walking`;
+    };
+    eat = function() {
+        function classic() {
+            console.log(this)
+        }
+        classic()
+      console.log(`${this.nameMine} is eating ${this.favoriteFood}`);
+    };
+    countBonuses() {
+        return this.#privateHuman.map(salary => salary > 200 ? salary * 1.5 : salary * 2);
+    }
+    #privateMethod() {
+        console.log(this.#privateHuman, 'I AM PRIVATE');
+    }
+    sleep() {
+        this.#privateMethod()
+      console.log(`${this.nameMine} went sleep for ${this.hoursOfSleep} hours`);
+    };
 
-//     walkAndEat() {
-//         this.eat()
-//         this.walk()
-//         console.log(this.#getIsParent())
-//       }
+    walkAndEat() {
+        this.eat()
+        this.walk()
+      }
 
-//     static parentStatic() {
-//         return 'I am parent static'
-//     }
-//   }
-  
-//   // Usage is still same:
-//   let human1 = new Human("Mykola", "Borcht");
+  }
+//   console.log(typeof Human)
+// //   // Usage is still same:
+  let human1 = new Human("Mykola", "Borcht");
+  human1.sleep()
+  console.log(human1.countBonuses())
+//   console.log(human1.#privateHuman)
+//   human1.eat()
 // //   human1.walk(999); // Mykola is walking
 // //   human1.eat(); // Mykola is eating Borcht
 //   human1.sleep(); // Mykola went sleep for null hours
@@ -147,25 +220,75 @@
 //   console.log(typeof new Human()); // object
 
 
-//   class Student extends Human {
-//       constructor({food, name, hOfSleep, favouriteSubj, firstLove, bestFfriend}){
-//           super(name, food, hOfSleep)
-//           this.favouriteSubj = favouriteSubj
-//           this.firstLove=firstLove
-//       }
+  class Student extends Human {
+    constructor({favFood, name,  hos, university, favSubject}){
+        super(name, favFood, hos)
+        this.university = university;
+        this.favSubject = favSubject
+    }
+    ageDescription = 'Teenager'
 
-//       getMyFavouriteSubject() {
-//           return this.favouriteSubj
-//       }
+    get hours() {
+        return this.hoursOfSleep
+    }
+ 
+    set hours(hos) {
+        this.hoursOfSleep = hos
+    }
 
-//     countLegsAndHands() {
-//         return this.legs + this.hands
-//     }
 
-//     static testStatic() {
-//         return 'I am static'
-//     }
+    reportAboutUni() {
+        console.log(`${this.nameMine} loves studying at ${this.university} and most of it is ${this.favSubject}`);
+    }
 
+    walk = () => {
+        // console.log(this.#privateField);
+        console.log(`${this.nameMine} walkt fast`);
+    }
+
+    static staticMethod() {
+        console.log('STATIC');
+        // DO NOT USE FOR WORK WITH OTHER METHODS OR THIS
+        this.staticInner()
+        Student.staticInner()
+    }
+
+    static staticInner() {
+        console.log('I can be called inside other static method')
+    }
+  }
+
+  const payload = {
+    university: 'KPI',
+    favSubject: 'Math',
+    favFood: "Deruny",
+    name: 'Vasya',
+    hos: 6
+  }
+
+const student1 = new Student({
+    university: 'KPI',
+    favSubject: 'Math',
+    favFood: "Deruny",
+    name: 'Vasya',
+    hos: 6
+  })
+student1.reportAboutUni()
+student1.walk()
+Student.staticMethod()
+
+// getter and setter
+console.log(student1.hours)
+student1.hours = 10
+console.log(student1.hours)
+// const myFunc = ({a,b,c,d,e}) =>{
+//     console.log(a,b,c,d,e)
+// }
+
+// const obj = {a: 1,b: 2,c: 3,d: 4,e: 5}
+// const {a,b,c,d,e} = obj
+// myFunc(obj)
+// human1.reportAboutUni()
 //   }
 //   const studObj = {
 //     food: "Varenyky", 
@@ -214,8 +337,14 @@
 //   let rabbit = {
 //     jumps: true
 //   };
+
+//   console.log(rabbit.jumps)
+//   console.log(rabbit.eats)
   
 //   rabbit.__proto__ = animal; // (*)
+
+//   console.log(rabbit.eats)
+//   console.log(rabbit)
   
 //   // тепер ми можемо знайти обидві властивості в об’єкті rabbit:
 //   console.log( rabbit.eats ); // true (**)
@@ -231,3 +360,44 @@
 //   console.log('    myStr    '.trim())
 // const num = 1;
 //   console.log(num.toString());
+
+
+// let animal = {
+//     eats: true
+//   };
+  
+//   function Rabbit(name) {
+//     this.name = name;
+//   }
+  
+//   console.log(Rabbit.prototype)
+//   Rabbit.prototype = animal;
+//   console.log(Rabbit.prototype)
+  
+//   let rabbit = new Rabbit("White Rabbit"); //  rabbit.__proto__ == animal
+  
+//   console.log( rabbit.eats ); 
+
+//   class Tabbit {
+//     constructor(name){
+//         this.name = name
+//     }
+//   }
+
+//   Tabbit.prototype = {constructor: animal}
+
+//   console.log(Tabbit.prototype)
+// const a = {abc: 'test'}
+
+// console.log(a.toString())
+
+// console.log(a.__proto__ === Object.prototype)
+
+// const num = 5;
+// // console.log(num.toString())
+
+// console.log(num.toString === Number.prototype.toString)
+
+
+// CHECK function in other dunction in class
+// Prototype from Class vs ConstructorFunction, reasignment to other object.
